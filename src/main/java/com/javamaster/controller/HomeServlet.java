cc5659e4ccb7e7b61189ff789066719bd6ac962d
Kinich-Zapote <kinichman@gmail.com>
2020-10-02 01:02:45 -0500

    Commit Siete

--- src/main/java/com/javamaster/controller/HomeServlet.java
+++ src/main/java/com/javamaster/controller/HomeServlet.java
@@ -1,18 +1,11 @@
 package com.javamaster.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.javamaster.dao.ArticleDao;
-import com.javamaster.dao.impl.ArticleDaoImpl;
-import com.javamaster.entity.Article;
-import com.javamaster.entity.Category;
 
 /**
  * Servlet implementation class HomeServlet
@@ -24,7 +17,7 @@ public class HomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = request.getServletPath();
         if (path.equals("/")) {
-            response.getWriter().println("<Resources>Hola</Resources>");
+            response.getWriter().println("Hey");
         }
 //        if (path.equals("/hello")) {
 //            request.response.sendRedirect("login.jsp");("/WEB-INF/view/hello.xml");
