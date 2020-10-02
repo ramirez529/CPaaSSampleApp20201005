2fa978aa53d79966482654773664d141a42c390b
Kinich-Zapote <kinichman@gmail.com>
2020-10-02 00:58:18 -0500

    Commit Cuatro

--- src/main/java/com/javamaster/controller/HomeServlet.java
+++ src/main/java/com/javamaster/controller/HomeServlet.java
@@ -18,10 +18,11 @@ import com.javamaster.entity.Category;
  * Servlet implementation class HomeServlet
  */
 public class HomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	private ArticleDao articleDao = new ArticleDaoImpl();
-       
+
+    private static final long serialVersionUID = 1L;
+
+    private ArticleDao articleDao = new ArticleDaoImpl();
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -30,25 +31,27 @@ public class HomeServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getServletPath();
-		if (path.equals("/")){
-			response.getWriter().println("Hello World");
-		}
-                else if (path.equals("/hello")){
-			request.getRequestDispatcher("/WEB-INF/view/hello.xml").forward(request, response);
-		}
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getServletPath();
+        if (path.equals("/")) {
+            response.getWriter().println("<Resources>Hola</Resources>");
+        }
+        if (path.equals("/hello")) {
+            request.response.sendRedirect("login.jsp");("/WEB-INF/view/hello.xml");
+        }
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
 
 }
